ab2f1f008f29c7bb4218a12d7517a257d0dfc2d3
mohjak@gmail.com <mohjak@outlook.com>
2016-02-24 12:06:56 +0200

    FizzBuzz commit.

---
+++ FizzBuzzMVC.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzzMVC;
+using FizzBuzzMVC.Controllers;
+
+namespace FizzBuzzMVC.Tests.Controllers
+{
+	[TestClass]
+	public class HomeControllerTest
+	{
+
+		[TestMethod]
+		public void Given1Return1()
+		{
+			FizzBuzzController controller = new FizzBuzzController();
+			ViewResult result = controller.Index(1) as ViewResult;
+			string expected = "1";
+			string actual = result.ViewBag.Output;
+			Assert.AreEqual(expected,actual);
+		}
+
+		[TestMethod]
+		public void Given3Return12Fizz()
+		{
+			FizzBuzzController controller = new FizzBuzzController();
+			ViewResult result = controller.Index(3) as ViewResult;
+			string expected = "1 2 Fizz ";
+			string actual = result.ViewBag.Output;
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Given5Return12Fizz4Buzz()
+		{
+			FizzBuzzController controller = new FizzBuzzController();
+			ViewResult result = controller.Index(5) as ViewResult;
+			string expected = "1 2 Fizz 4 Buzz ";
+			string actual = result.ViewBag.Output;
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Given15ReturnFizzBuzz()
+		{
+			FizzBuzzController controller = new FizzBuzzController();
+			ViewResult result = controller.Index(15) as ViewResult;
+			string expected = "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz ";
+			string actual = result.ViewBag.Output;
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Index()
+		{
+			// Arrange
+			HomeController controller = new HomeController();
+
+			// Act
+			ViewResult result = controller.Index() as ViewResult;
+
+			// Assert
+			Assert.IsNotNull(result);
+		}
+
+		[TestMethod]
+		public void About()
+		{
+			// Arrange
+			HomeController controller = new HomeController();
+
+			// Act
+			ViewResult result = controller.About() as ViewResult;
+
+			// Assert
+			Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+		}
+
+		[TestMethod]
+		public void Contact()
+		{
+			// Arrange
+			HomeController controller = new HomeController();
+
+			// Act
+			ViewResult result = controller.Contact() as ViewResult;
+
+			// Assert
+			Assert.IsNotNull(result);
+		}
+	}
+}
---
+++ FizzBuzzMVC/Controllers/FizzBuzzController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FizzBuzzMVC.Controllers
+{
+	public class FizzBuzzController : Controller
+	{
+		// GET: FizzBuzz
+		public ActionResult Index(int value)
+		{
+			for (int i = 1; i <= value; i++)
+			{
+				if (i % 3 == 0 && i % 5 == 0)
+				{
+					ViewBag.Output += "FizzBuzz ";
+				}
+				else if (i % 5 == 0)
+				{
+					ViewBag.Output += "Buzz ";
+				}
+				else if (i % 3 == 0)
+				{
+					ViewBag.Output += "Fizz ";
+				}
+				else
+				{
+					ViewBag.Output += i.ToString() + " ";
+				}
+			}
+			return View();
+		}
+	}
+}
